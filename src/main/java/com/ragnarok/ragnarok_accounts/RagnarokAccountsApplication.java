4f17c49bcfeb165f0aef405b3913706619c7334a
sambit950 <sambit.panda@pncilab.com>
2026-09-18 08:58:14 +0000

    intial commit

---
+++ src/main/java/com/ragnarok/ragnarok_accounts/RagnarokAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.ragnarok.ragnarok_accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagnarokAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagnarokAccountsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ragnarok/ragnarok_accounts/RagnarokAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ragnarok.ragnarok_accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagnarokAccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
